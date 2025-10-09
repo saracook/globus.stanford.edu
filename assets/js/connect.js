728341391dda6953536745aab5874598bb04efd9
saracook <saracook@stanford.edu>
2025-10-09 15:39:20 -0700

    fixing accessiblity issues in form

--- assets/js/connect.js
+++ assets/js/connect.js
@@ -15,6 +15,10 @@ $(document).ready(function() {
     label.className = "sr-only";
     label.htmlFor = "bucket" + index;
     label.textContent = "Bucket";
+    const labelSelect = document.createElement("label");
+    labelSelect.className = "sr-only";
+    labelSelect.htmlFor = "permissions" + index;
+    label.textContent = "Permissions";
     const newFieldset = document.createElement("div");
     newFieldset.prepend(label);
     newFieldset.className = "row-added input-group row-" + index;
